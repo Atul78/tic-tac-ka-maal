11c5a6df9dcb6e194335f8db23f491995bdc7d6f
Atul78 <83898615+Atul78@users.noreply.github.com>
2022-08-23 18:32:50 +0530

    Update index.js

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,49 +1,64 @@
 var val = 3;
 var count = 1;
-var m = document.getElementById("container2");
 let player1 = "X";
 let player2 = "O";
-function myFunction() {
+const arr = [null];
+
+function Matrix() {
 
     console.log(val);
     let key = 1;
     for(let i = 1; i <= val; i++) {
         var parent = document.createElement("div");
-        var b = document.getElementById("container2");
+        var container2 = document.getElementById("container2");
         parent.classList.add("prnt");
-        b.appendChild(parent)
+        container2.appendChild(parent)
         for(let j = 1; j <= val; j++) {
-            var a = document.createElement("div");
-            a.classList.add("box");
-            a.setAttribute('id',`${j}${i}`);
-            
-
-            let z = parent.appendChild(a);
+            var div = document.createElement("div");
+            div.classList.add("box");
+            div.setAttribute('id',`${j}${i}`);
+            let z = parent.appendChild(div);
             // function hi() {
                 //     for(let f = 0; f < val*val; f++) {
                     //         if(k.id == "X")
                     //     }
                     // }
+                    console.log(`${j}${i}`)
                     
-                a.addEventListener('click', () => {
+                div.addEventListener('click', () => {
                 let k = document.getElementById(`${j}${i}`);
-                console.log(k)
+                console.log(k.id);
+                const arr = [];
                 if(z.innerText == '') {
                     if(count % 2 == 0) {
-                        z.innerText = player1;  
+                        let a = z.innerText = player1;
+                        // z.innerText = player1;
+                        console.log(a);
                         count++;
+                        Store(arr, a);
+
+                        // if(k.id == `${j}${i}` && k.id == `${j}${i+1}` && k.id == `${j}${i+2}` ) {
+                        //     console.log("player1 is winner")
+                        // }
                 }
                 else {
-                    z.innerText = player2;
+                    let b = z.innerText = player2;
+                    console.log(b)
                     count++;
                 }
-                }
-            
-                    
-           
+                }        
             })
         }
         
     }
+}
+
+function Store(arr, variable) {
+    for(let i = 0; i < val; i++) {
+        for(let j = 0; j < val; j++) {
+            arr[j] = [variable];
+        }
+    }
+    console.log(arr)
 }
 
